798273f481e23915894b3775dfbbd34d894fcd7c
M-Ankitha <122814756+M-Ankitha@users.noreply.github.com>
2023-12-08 17:10:16 +0530

    Update News.js

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -12,36 +12,66 @@ const News = () => {
     setMyNews(data.articles);
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
 
-  return (
-    <>
+//   return (
+//     <>
+//     <h1 className="text-center my-3">Enjoy Daily Top - Headlines</h1>
+//           <div className="mainDiv">
+//       {mynews.map((ele) => {
+//         console.log(ele)
+//         return (
+//           <>
+//         <div class="card" style={{  marginTop:"2rem" , boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px"}}>
+//               <img src={ele.urlToImage == null ? "https://kubrick.htvapps.com/vidthumb/f6865cb1-d77d-4a31-ba83-d57c4b2324d8/4b9c9d8f-ad14-47ea-bcf4-bf24ee0bb1f3.jpg?crop=0.383xw:0.383xh;0.517xw,0.252xh&resize=1200:*" : ele.urlToImage} class="card-img-top" alt="..." />
+//               <div class="card-body">
+//                 <h5 class="card-title">{ele.author == "" ? "Janelle Ash" : ele.author}</h5>
+//                 <p class="card-text">
+//                  {ele.title}
+//                 </p>
+//                 <a href={ele.url} target="_blank" class="btn btn-primary">
+//                   Read More
+//                 </a>
+//               </div>
+//             </div>
+//           </>
+//         );
+//     })}
+//     </div>
+//     </>
+//   );
+// };
+  useEffect(() => {
+  fetchData();
+}, []);
+
+return (
+  <>
     <h1 className="text-center my-3">Enjoy Daily Top - Headlines</h1>
-          <div className="mainDiv">
-      {mynews.map((ele) => {
-        console.log(ele)
-        return (
-          <>
-        <div class="card" style={{  marginTop:"2rem" , boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px"}}>
-              <img src={ele.urlToImage == null ? "https://kubrick.htvapps.com/vidthumb/f6865cb1-d77d-4a31-ba83-d57c4b2324d8/4b9c9d8f-ad14-47ea-bcf4-bf24ee0bb1f3.jpg?crop=0.383xw:0.383xh;0.517xw,0.252xh&resize=1200:*" : ele.urlToImage} class="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">{ele.author == "" ? "Janelle Ash" : ele.author}</h5>
-                <p class="card-text">
-                 {ele.title}
-                </p>
-                <a href={ele.url} target="_blank" class="btn btn-primary">
-                  Read More
-                </a>
-              </div>
+    <div className="mainDiv">
+      {mynews ? (
+        mynews.map((ele) => (
+          <div className="card" style={{ marginTop: "2rem", boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px" }}>
+            <img src={ele.urlToImage == null ? "https://kubrick.htvapps.com/vidthumb/f6865cb1-d77d-4a31-ba83-d57c4b2324d8/4b9c9d8f-ad14-47ea-bcf4-bf24ee0bb1f3.jpg?crop=0.383xw:0.383xh;0.517xw,0.252xh&resize=1200:*" : ele.urlToImage} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{ele.author === "" ? "Jeff cox" : ele.author}</h5>
+              <p className="card-text">
+                {ele.title}
+              </p>
+              <a href={ele.url} target="_blank" className="btn btn-primary">
+                Read More
+              </a>
             </div>
-          </>
-        );
-    })}
+          </div>
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
-    </>
-  );
+  </>
+);
 };
 
 export default News;
